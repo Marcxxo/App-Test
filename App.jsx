88427e08a595705f11e63ead039694fd0d9615b3
Marcxxo <ochsenreiter.marco@gmail.com>
2025-05-22 18:45:16 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import ProfileForm from './components/ProfileForm'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Neue Visitenkarte erstellen</h1>
+      <ProfileForm />
+    </div>
+  )
+}
+
+export default App
